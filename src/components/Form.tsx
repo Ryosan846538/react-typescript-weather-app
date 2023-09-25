e5fd719435be67e443a105c7cdb2c3877fdbb344
Ryota Asai <ryota@dot1xh3974.14-176-20.wlan.ms.dendai.ac.jp>
2023-09-25 17:04:27 +0900

    update From.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
 const Form = () => {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState<string>("");
+  const getWeather = () => {
+    fetch(
+      "https://api.weatherapi.com/v1/current.json?key=c0637c71ae8448ee8f175640232509&q=London&aqi=no"
+    ).then((res) => console.log(res));
+  };
   return (
     <form>
       <input
@@ -10,8 +15,9 @@ const Form = () => {
         placeholder="都市名"
         onChange={(e) => setCity(e.target.value)}
       />
-      {city}
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>
+        Get Weather
+      </button>
     </form>
   );
 };
